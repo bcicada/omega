49dc4ef3f7422cf259cbcc33ca99b3b152627180
bcicada <b.cicada@protonmail.com>
2023-01-13 03:49:24 +0000

    Adding Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,17 @@ import { ConnectedComponent } from './auth/connected/connected.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { ManifestComponent } from './pages/manifest/manifest.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -36,9 +47,21 @@ import { ManifestComponent } from './pages/manifest/manifest.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+	production: false,
+  firebase: {
+    projectId: 'omega-cnam',
+    appId: '1:525140447583:web:0bcedda13a265ea339e6ea',
+    storageBucket: 'omega-cnam.appspot.com',
+    apiKey: 'AIzaSyBUvt0jYA3LWPKYAu4080J4FePG1PClYBE',
+    authDomain: 'omega-cnam.firebaseapp.com',
+    messagingSenderId: '525140447583',
+    measurementId: 'G-Y9F460QJ3E',
+  }
+}
